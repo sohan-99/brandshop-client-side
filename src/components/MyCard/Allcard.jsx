511ab020e5631e408548b4b69bd63289dbe5e690
sohan-99 <sohan75632@gmail.com>
2023-10-21 11:28:24 +0600

    deleted in ui in my cardData

--- src/components/MyCard/Allcard.jsx
+++ src/components/MyCard/Allcard.jsx
@@ -4,7 +4,7 @@
 import Swal from "sweetalert2";
 
 
-const Allcard = ({ item }) => {
+const Allcard = ({ item,setRefetch }) => {
     const { _id, name } = item;
 
     const handleDelet = (_id) => {
@@ -32,6 +32,7 @@ const Allcard = ({ item }) => {
                                 'YourCart has been deleted.',
                                 'success'
                             )
+                            setRefetch(true)
                         }
                     })
             }
--- src/components/MyCard/MyCard.jsx
+++ src/components/MyCard/MyCard.jsx
@@ -1,17 +1,25 @@
 /* eslint-disable no-undef */
-import { useLoaderData } from "react-router-dom";
+// import { useLoaderData } from "react-router-dom";
 import Navber from "../Navber/Navber";
 import Allcard from "./Allcard";
+import { useEffect, useState } from "react";
 
 const MyCard = () => {
-    const lodercardData = useLoaderData();
+    // const lodercardData = useLoaderData();
+    const [cardData, setCardData] = useState([]);
+    const [reFetch, setRefetch] = useState(false);
+    useEffect(()=>{
+        fetch('http://localhost:5000/mycart')
+        .then(res=>res.json())
+        .then(data=>{setCardData(data)})
+    },[reFetch])
     // console.log(lodercardData);
     return (
         <div className="my-6">
             <Navber></Navber>
             <div className="grid lg:grid-cols-2 gap-4 mt-6">
                 {
-                    lodercardData.map((item, index) => <Allcard key={index} item={item}></Allcard>)}
+                    cardData?.map((item, index) => <Allcard key={index} item={item} setRefetch={setRefetch}></Allcard>)}
             </div>
         </div>
     );
