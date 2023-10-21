ec94c271b55157b26521cc282179b40f366d176a
sohan-99 <sohan75632@gmail.com>
2023-10-21 15:17:49 +0600

    Dark mode with Toggle Button in navbar

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
     return (
-        <div className="max-w-7xl mx-auto">
+        <div className="max-w-full mx-auto">
 
             <Outlet></Outlet>
         </div>
