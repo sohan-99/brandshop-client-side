050ccfc83afdb04f584d679c2be8b3639917ba58
sohan-99 <sohan75632@gmail.com>
2023-10-21 14:50:17 +0600

    Add important announcement message for Vivo users

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -29,19 +29,41 @@ const Products = () => {
                                 <h3 className="lg:text-2xl lg:font-semibold"> Product Rating:{product.rating}</h3>
                             </div>
                             <div className="flex justify-around ">
-
-                            <Link to={`/update/${product._id}`}>
-                                <button className="btn btn-secondary text-center">Update </button>
-                            </Link>
-                            <Link to={`/details/${product._id}`}>
-                                <button className="btn btn-secondary text-center">Details</button>
-                            </Link>
+                                <Link to={`/update/${product._id}`}>
+                                    <button className="btn btn-secondary text-center">Update </button>
+                                </Link>
+                                <Link to={`/details/${product._id}`}>
+                                    <button className="btn btn-secondary text-center">Details</button>
+                                </Link>
                             </div>
                         </div>
                     ))
                 }
-            </div>
+                {!products.length &&
+                    <div className="text-center lg:w-1/2 md:w-3/4 w-full mx-auto">
+                        <h3 className="text-lg lg:text-2xl mt-6 mb-3">
+                            🌟 Important Announcement for Vivo Users 🌟
+                        </h3>
+                        <h2 className="text-xl lg:text-2xl mb-3">
+                            Dear Vivo Community,
+                        </h2>
+                        <p className="text-base lg:text-lg mb-4">
+                            We are thrilled to announce that we are working tirelessly to bring you an exciting new software update! This update is designed to enhance your overall Vivo experience with improved performance, new features, and enhanced security. We appreciate your patience and support as we strive to deliver the best for you.
+                        </p>
+                        <h4 className="text-lg lg:text-xl mb-4">
+                            Stay tuned for more updates and keep enjoying your Vivo devices!
+                        </h4>
+                        <h3 className="text-xl lg:text-xl mb-1">
+                            Best regards,
+                        </h3>
+                        <h3 className="text-xl lg:text-xl">
+                            The Vivo Team
+                        </h3>
+                    </div>
+                
+}
         </div>
+        </div >
     );
 };
 
